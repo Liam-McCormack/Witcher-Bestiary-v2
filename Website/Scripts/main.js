c6efbb42a9ae1f105705911fcde4fe505b45fa9b
Liam <liammc12@hotmail.co.uk>
2020-09-24 12:14:05 +0100

    changed Enter button function to redirect to site

--- Website/Scripts/main.js
+++ Website/Scripts/main.js
@@ -1,7 +1,7 @@
 
 //Homepage Enter
 function enterButton() {
-    location.href='http://127.0.0.1:5500/Website/mainPage.html';
+    location.href='../mainPage.html';
 }
 
 document.getElementById("enterButton").addEventListener("click", enterButton);
